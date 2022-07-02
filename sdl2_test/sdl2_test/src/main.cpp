addc3ea121b34b37ded33d8c89489fbbf1dc12f1
4yvakZ <35529078+4yvakZ@users.noreply.github.com>
2022-07-02 12:25:08 +0300

    Glad added and simple openGL render

--- sdl2_test/sdl2_test/src/main.cpp
+++ sdl2_test/sdl2_test/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <glad/glad.h>
 
 int main(int argc, char* argv[]) {
 
@@ -12,11 +13,28 @@ int main(int argc, char* argv[]) {
 		std::cout << "SDL video system is ready to go\n";
 	}
 
-	window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	window = SDL_CreateWindow("SDL2 Test",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context;
+	context = SDL_GL_CreateContext(window);
+
+	gladLoadGLLoader(SDL_GL_GetProcAddress);
+
 
 	bool gameIsRunning = true;
-
 	while (gameIsRunning) {
+		glViewport(0, 0, 640, 480);
 		SDL_Event event;
 
 		while (SDL_PollEvent(&event)) {
@@ -33,6 +51,11 @@ int main(int argc, char* argv[]) {
 			default:
 				break;
 			}
+
+			glClearColor(1, 0, 0, 1);
+			glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+			SDL_GL_SwapWindow(window);
 		}
 	}
 
